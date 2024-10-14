2e0541f53d3b883a1761614673e575729c2ddb0f
Berkay Gündüz <bby.gndz@gmail.com>
2024-10-14 16:14:11 +0300

    new pages

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ListingController extends Controller
+{
+    //
+    public function create(Request $request) {
+        return view('listings.create');
+    }
+}
