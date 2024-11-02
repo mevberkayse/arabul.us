12a88c9a00e89240f0d620a22cbc2cdd0d2517cb
Berkay Gündüz <bby.gndz@gmail.com>
2024-11-02 16:29:33 +0300

    Update homepage.blade.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,7 +7,7 @@
     <title>AraBulus</title>
     <link rel="stylesheet" href="//cdn.arabul.us/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/css/homepage.css?{{time();}}">
-    
+
 </head>
 
 <body>
@@ -24,19 +24,22 @@
                         aria-label="Search">
                     <button class="btn btn-outline-success" type="submit">Search</button>
                 </form>
-                <div class="d-flex align-items-center ">            
+                <div class="d-flex align-items-center ">
                     <div class="border rounded p-2 d-flex align-items-center">
                         <i class="fa-solid fa-map-marker-alt"></i>
                         <span class="ms-2">Gaziantep, Türkiye</span>
                         <div class="dropdown ms-2">
-                            <i class="fa-solid fa-chevron-down dropdown-toggle" id="locationDropdown" data-bs-toggle="dropdown" aria-expanded="false"></i>
+                            <i class="fa-solid fa-chevron-down dropdown-toggle" id="locationDropdown"
+                                data-bs-toggle="dropdown" aria-expanded="false"></i>
                             <ul class="dropdown-menu" aria-labelledby="locationDropdown">
-                            <li>
-                                <div class="input-group">
-                                    <span class="input-group-text border-0 bg-transparent"><i class="fa-solid fa-magnifying-glass"></i></span>
-                                    <input type="text" class="form-control" placeholder="İl ve İlçe Gir" aria-label="Search">
-                                </div>
-                            </li>
+                                <li>
+                                    <div class="input-group">
+                                        <span class="input-group-text border-0 bg-transparent"><i
+                                                class="fa-solid fa-magnifying-glass"></i></span>
+                                        <input type="text" class="form-control" placeholder="İl ve İlçe Gir"
+                                            aria-label="Search">
+                                    </div>
+                                </li>
                                 <li>
                                     <a class="dropdown-item text-primary transparent-button" href="#" role="button">
                                         Mevcut Konum Kullan
@@ -45,45 +48,44 @@
                             </ul>
                         </div>
                     </div>
-</div>
-</div>
-</div>
-        
-                <div class="d-flex gap-5 align-items-center">
-                    <a href="#" class="btn btn-outline-dark">
-                        <i class="fa-solid fa-plus"></i>
-                    </a>
-                    <div class="flex-shrink-0 dropdown">
-                        <a href="#" class="btn btn-outline-light" data-bs-toggle="dropdown" aria-expanded="false">
-                            <i class="fa-solid fa-moon text-dark"></i>
-                        </a>
-                        <ul class="dropdown-menu text-small shadow">
-                            <li><a class="dropdown-item" href="#">
-                                    <i class="fa-solid fa-moon"></i>
-                                    Dark</a></li>
-                            <li><a class="dropdown-item" href="#">
-                                    <i class="fa-solid fa-sun"></i>Light</a></li>
-                            <li><a class="dropdown-item" href="#">
-                                    <i class="fa-solid fa-circle-half-stroke"></i>
-                                    Auto</a></li>
-                        </ul>
-                    </div>
-                    <div class="flex-shrink-0 dropdown">
-                        <a href="#" class="d-block link-body-emphasis text-decoration-none" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32"
-                                class="rounded-circle">
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-lg-end dropdown-menu-start text-small shadow">
-                            <li><a class="dropdown-item" href="#">Profile</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Sign out</a></li>
-                        </ul>
-                    </div>
                 </div>
             </div>
+        </div>
+
+        <div class="d-flex gap-5 align-items-center">
+            <a href="#" class="btn btn-outline-dark">
+                <i class="fa-solid fa-plus"></i>
+            </a>
+            <div class="flex-shrink-0 dropdown">
+                <a href="#" class="btn btn-outline-light" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fa-solid fa-moon text-dark"></i>
+                </a>
+                <ul class="dropdown-menu text-small shadow">
+                    <li><a class="dropdown-item" href="#">
+                            <i class="fa-solid fa-moon"></i>
+                            Dark</a></li>
+                    <li><a class="dropdown-item" href="#">
+                            <i class="fa-solid fa-sun"></i>Light</a></li>
+                    <li><a class="dropdown-item" href="#">
+                            <i class="fa-solid fa-circle-half-stroke"></i>
+                            Auto</a></li>
+                </ul>
+            </div>
+            <div class="flex-shrink-0 dropdown">
+                <a href="#" class="d-block link-body-emphasis text-decoration-none" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+                </a>
+                <ul class="dropdown-menu dropdown-menu-lg-end dropdown-menu-start text-small shadow">
+                    <li><a class="dropdown-item" href="#">Profile</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="#">Sign out</a></li>
+                </ul>
+            </div>
+        </div>
+        </div>
         </div>
     </nav>
 
@@ -163,35 +165,9 @@
     </nav>
     <div class="container mt-5">
         <div class="row justify-content-center gap-0 ">
-        <div class="col-3 ">
-                <div class="card" style="width: 18rem;">
-                    <img src="assets/images/shopping.png" class="card-img-top img-small" alt="...">
-                    <div class="card-body">
-                        <h5 class="item-price large-price">99.999 TL</h5>
-                        <p class="item-text mt-2">İphone 16 Yeni</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <p class="location">Gaziantep</p>
-                            <p class="time">Dün</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div class="col-3 ">
                 <div class="card" style="width: 18rem;">
                     <img src="assets/images/shopping.png" class="card-img-top img-small" alt="...">
-                    <div class="card-body">
-                        <h5 class="item-price large-price">99.999 TL</h5>
-                        <p class="item-text mt-2">İphone 16 Yeni</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <p class="location">Gaziantep</p>
-                            <p class="time">Dün</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-3 ">
-                <div class="card" style="width: 18rem;">
-                <img src="assets/images/shopping.png" class="card-img-top img-small" alt="...">
                     <div class="card-body">
                         <h5 class="item-price large-price">99.999 TL</h5>
                         <p class="item-text mt-2">İphone 16 Yeni</p>
@@ -215,22 +191,48 @@
                     </div>
                 </div>
             </div>
+            <div class="col-3 ">
+                <div class="card" style="width: 18rem;">
+                    <img src="assets/images/shopping.png" class="card-img-top img-small"  alt="...">
+                    <div class="card-body">
+                        <h5 class="item-price large-price">99.999 TL</h5>
+                        <p class="item-text mt-2">İphone 16 Yeni</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <p class="location">Gaziantep</p>
+                            <p class="time">Dün</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="text-center dahafazlayukle-btn my-4">
-                <button class="btn custom-btn btn-primary">Daha Fazla Yükle</button>
-            </div>
-
-            <!-- Footer -->
-            <footer class="bg-light text-center text-lg-start mt-auto py-3 fixed-buttom">
-                <div class="container">
-                    <p class="text-muted mb-0">&copy; 2024 Şirket Adı. Tüm hakları saklıdır.</p>
+            <div class="col-3 ">
+                <div class="card" style="width: 18rem;">
+                    <img src="assets/images/shopping.png" class="card-img-top img-small" alt="...">
+                    <div class="card-body">
+                        <h5 class="item-price large-price">99.999 TL</h5>
+                        <p class="item-text mt-2">İphone 16 Yeni</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <p class="location">Gaziantep</p>
+                            <p class="time">Dün</p>
+                        </div>
+                    </div>
                 </div>
-</footer>
+            </div>
+        </div>
+    </div>
+    <div class="text-center dahafazlayukle-btn my-4">
+        <button class="btn custom-btn btn-primary">Daha Fazla Yükle</button>
+    </div>
+
+    <!-- Footer -->
+    <footer class="bg-light text-center text-lg-start mt-auto py-3 fixed-bottom">
+        <div class="container">
+            <p class="text-muted mb-0">&copy; 2024 Şirket Adı. Tüm hakları saklıdır.</p>
+        </div>
+    </footer>
 
 
-            <script src="//cdn.arabul.us/bootstrap/js/bootstrap.bundle.min.js"></script>
-            <script src="//cdn.arabul.us/fontawesome/js/all.min.js"></script>
+    <script src="//cdn.arabul.us/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="//cdn.arabul.us/fontawesome/js/all.min.js"></script>
 </body>
 
 </html>
