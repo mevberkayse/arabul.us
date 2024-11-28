349f5437637c9128eed1c55860b3116cecd455f6
Berkay Gündüz <bby.gndz@gmail.com>
2024-11-28 16:25:40 +0300

    search & search filter functions

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,12 +4,32 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Listing;
+use App\Models\Category;
 class SearchController extends Controller
 {
     //
-    public function index(Request $request, $query)
+    public function index(Request $request)
     {
-        return view('search.results');
+        $query = $request->input('query');
+        $listings = Listing::where('title', 'LIKE', '%' . $query . '%')->orWhere('descriptions', 'LIKE', '%' . $query . '%')->get();
+        // get $listings items categories
+        $categories = [];
+        foreach($listings as $listing) {
+            $categories[] = Category::where('id', $listing->category_id)->first();
+        }
+
+        // filter out duplicate categories
+        $categories = array_unique($categories);
+
+        // get parameters for each category, put them in an array and remove duplicates
+        $parameters = [];
+        foreach($categories as $category) {
+            $parameters[] = $category->getParameters();
+        }
+        $parameters = array_unique($parameters, SORT_REGULAR);
+
+
+        return view('listings.search_results', ['listings' => $listings, 'categories' => $categories, 'parameters' => $parameters]);
     }
 }
