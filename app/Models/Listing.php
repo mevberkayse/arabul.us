f99a2e89c728365a9abc0d8177080be21d8c7a24
Berkay Gündüz <bby.gndz@gmail.com>
2024-12-07 14:42:06 +0300

    Update Listing.php

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -59,7 +59,7 @@ class Listing extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id', 'id');
+        return $this->belongsTo(User::class);
     }
 
     public function getSlug()
